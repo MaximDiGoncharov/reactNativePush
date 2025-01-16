c3a90f903927af2c75ffa581d60dc353170f8403
MaxPups <max_pups@bk.ru>
2025-01-16 21:34:54 +0300

    добавил файл для отправки уведомлений, обернул все приложение в webView

--- App.js
+++ App.js
@@ -1,51 +1,14 @@
-import React, {useEffect, useState} from "react";
-import {View, Text, TouchableOpacity, StyleSheet, Alert} from "react-native";
-import * as Clipboard from "expo-clipboard";
+import { WebView } from 'react-native-webview';
 import {useExpoPushToken} from './hooks/expoPushToken';
+
 
 const App = () => {
    const deviceToken = useExpoPushToken();
    return (
-      <View style={styles.container}>
-         <Text style={styles.title}>Push Token:</Text>
-         <Text style={styles.token}>{deviceToken || "Fetching token..."}</Text>
-      </View>
+      <WebView
+        source={{ uri: `https://universal.laxo.one/enter/${deviceToken ?? null}`  }}
+      />
    );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
-    backgroundColor: "#f5f5f5",
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-  token: {
-    fontSize: 14,
-    color: "#333",
-    marginBottom: 20,
-    textAlign: "center",
-  },
-  button: {
-    backgroundColor: "#007bff",
-    padding: 10,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 16,
-  },
-  warning: {
-    color: "red",
-    fontSize: 14,
-    marginTop: 20,
-  },
-});
 
 export default App;
